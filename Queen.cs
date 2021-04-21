27a4c6da57d04d80bdf96d932598dad4fdf3da2b
Iván Ezequiel Iencinella <iiencinella@outlook.es>
2021-04-20 22:48:07 -0300

    Agregar archivos de proyecto.

---
+++ Queen.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace eight_queen
+{
+  class Queen
+  {
+    private int col;
+    private int row;
+
+    public Queen() { col = 0; row = 0; }
+
+    public int Column
+    {
+      set { col = value; }
+      get { return col; }
+    }
+
+    public int Row
+    {
+      set { row = value; }
+      get { return row; }
+    }
+    public int getColRowAdd()
+    {
+      return col + row;
+    }
+
+    public int getColRowSub()
+    {
+      return col - row;
+    }
+
+    public int getRowColSub()
+    {
+      return row - col;
+    }
+  }
+}
---
+++ README.txt
@@ -0,0 +1,1 @@
+﻿Este proyecto se desarrolla para poder evaluar si una solución es válido o no para resolver el problema de N Reinas en un tablero de NxN dimensiones.
